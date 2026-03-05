3d39f741ccacdf91bcf5ce50f464c9f9327bc134
QuirijnVanDerZanden <quirijn.vanderzanden@hotmail.com>
2026-03-05 10:21:09 +0100

    Groceries GroupByCategory

--- Groceries-list-planner/src/groceries_list_planner/ingredients.clj
+++ Groceries-list-planner/src/groceries_list_planner/ingredients.clj
@@ -1,11 +1,13 @@
 (ns groceries-list-planner.ingredients)
 
+;; Scale ingredients with the amount of people that will eat the recipe on that day
 (defn scale-ingredients [recipe people]
   (let [ratio (/ people (:servings recipe))]
     (map (fn [ingredient]
            (update ingredient :amount * ratio))
          (:ingredients recipe))))
 
+;; Puts all ingredients from each day into a single list for the week
 (defn week-plan-ingredients [recipes week-plan]
   (if (empty? week-plan)
     []
@@ -13,6 +15,7 @@
           ings (scale-ingredients (get recipes (:recipe day)) (:people day))]
       (concat ings (week-plan-ingredients recipes (rest week-plan))))))
 
+;; Aggregates ingredients by name, unit and category
 (defn aggregate-ingredients [ingredients]
   (let [key-fn (fn [{:keys [name unit category]}]
                  [name unit category])]
@@ -21,3 +24,7 @@
          (map (fn [[_ group]]
                 (let [total (reduce + (map :amount group))]
                   (assoc (first group) :amount total)))))))
+
+;; Groups ingredients by category
+(defn group-by-category [ingredients]
+  (group-by :category ingredients))
