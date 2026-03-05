3e5c3786b67b7417b29b5d5941e16b3eefbabfb1
QuirijnVanDerZanden <quirijn.vanderzanden@hotmail.com>
2026-03-05 09:08:34 +0100

    Groceries IngredientAggregate

--- Groceries-list-planner/src/groceries_list_planner/ingredients.clj
+++ Groceries-list-planner/src/groceries_list_planner/ingredients.clj
@@ -12,3 +12,12 @@
     (let [day (first week-plan)
           ings (scale-ingredients (get recipes (:recipe day)) (:people day))]
       (concat ings (week-plan-ingredients recipes (rest week-plan))))))
+
+(defn aggregate-ingredients [ingredients]
+  (let [key-fn (fn [{:keys [name unit category]}]
+                 [name unit category])]
+    (->> ingredients
+         (group-by key-fn)
+         (map (fn [[_ group]]
+                (let [total (reduce + (map :amount group))]
+                  (assoc (first group) :amount total)))))))
