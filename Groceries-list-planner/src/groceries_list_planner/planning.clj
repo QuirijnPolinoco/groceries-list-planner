6a29eb88b3c6e2d993653ad40eced96b624f6abd
QuirijnVanDerZanden <quirijn.vanderzanden@hotmail.com>
2026-03-06 14:47:05 +0100

    Groceries RemoveHardcodedWeekPlan

--- Groceries-list-planner/src/groceries_list_planner/planning.clj
+++ Groceries-list-planner/src/groceries_list_planner/planning.clj
@@ -2,12 +2,3 @@
 
 (def week-days
   [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
-
-(def week-plan
-  [{:day :monday    :recipe :meat-pasta     :people 2}
-   {:day :tuesday   :recipe :green-curry    :people 4}
-   {:day :wednesday :recipe :tacos          :people 3}
-   {:day :thursday  :recipe :chicken-orzo   :people 2}
-   {:day :friday    :recipe :chicken-gnocchi :people 4}
-   {:day :saturday  :recipe :honey-chicken  :people 2}
-   {:day :sunday    :recipe :chicken-pitas  :people 2}])
