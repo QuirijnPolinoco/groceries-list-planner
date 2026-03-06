df53df353053cddeb296c8462b47b73bbf15188f
QuirijnVanDerZanden <quirijn.vanderzanden@hotmail.com>
2026-03-06 11:53:00 +0100

    Groceries CollectAndFormat

--- Groceries-list-planner/src/groceries_list_planner/core.clj
+++ Groceries-list-planner/src/groceries_list_planner/core.clj
@@ -4,6 +4,21 @@
             [groceries-list-planner.recipe :as recipe]
             [groceries-list-planner.planning :as planning]))
 
+(declare print-recipes recipe-name->id-map collect-week-plan format-week-plan)
+
+(defn -main [& _]
+  (println "Type 'start' to begin planning your week")
+  (let [input (str/trim (read-line))]
+    (when (= (str/lower-case input) "start")
+      (print-recipes)
+      (let [name->id (recipe-name->id-map recipe/recipes)
+            week-plan (collect-week-plan name->id)]
+        (format-week-plan week-plan recipe/recipes)
+        (println)))))
+
+(defn run []
+  (-main))
+
 (defn printing-shopping-list [shopping-list]
   (doseq [cat (sort (keys shopping-list))]
     (println (str "== " (name cat) " =="))
@@ -30,3 +45,24 @@
           people (when people-part (parse-long people-part))]
       (when (and recipe-id people (pos? people))
         {:recipe recipe-id :people people}))))
+
+(defn collect-week-plan
+  ([name->id] (collect-week-plan name->id planning/week-days []))
+  ([name->id days plan]
+   (if (empty? days)
+     plan
+     (let [day (first days)
+           day-name (str/capitalize (name day))
+           _ (println (str "What would you like to eat on " day-name " and with how many?"))
+           input (str/trim (read-line))
+           meal (meal-input input name->id)]
+       (if meal
+         (recur name->id (rest days) (conj plan (assoc meal :day day)))
+         (do (println "Invalid input. Use format: Recipe name, number")
+             (recur name->id days plan)))))))
+
+(defn format-week-plan [week-plan recipes]
+  (doseq [meal week-plan]
+    (let [day-name (str/capitalize (name (:day meal)))
+          recipe-name (get-in recipes [(:recipe meal) :name])]
+      (println (str day-name ": " recipe-name " (" (:people meal) " people)")))))
